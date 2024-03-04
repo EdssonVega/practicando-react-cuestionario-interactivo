ae882146350e0d7a98c1efb4fc97ddf969eb21a6
Edsson <e_d_sx2@hotmail.com>
2024-03-04 15:19:17 -0600

    creado componente navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Navbarsection from './components/navbar.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hola
+      <Navbarsection />
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./navbar.css";
+
+function Navbarsection (){
+    return(
+    <section className="navbar">
+        <p>Dynamic <b>Form</b></p>
+        <p>Signed in as: <u>Edsson Vega</u></p>
+    </section>
+    )
+}
+
+export default Navbarsection
